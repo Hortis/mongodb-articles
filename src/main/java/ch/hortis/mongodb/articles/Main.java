bfb77347996f128c6887a02c97acdf9e27f3f82f
Ivan Hristov <ivan.hristov@hortis.ch>
2014-01-21 09:50:22 +0100

    initial commit

---
+++ src/main/java/ch/hortis/mongodb/articles/Main.java
@@ -0,0 +1,59 @@
+package ch.hortis.mongodb.articles;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
+import org.glassfish.jersey.server.TracingConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://localhost:8080/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     *
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in ch.hortis package
+        final ResourceConfig rc = new ResourceConfig()
+                .property(ServerProperties.TRACING, TracingConfig.ON_DEMAND.name())
+                .registerInstances(new ServiceBinder()) // HK2 dependency injection mapping
+                .packages(true, "ch.hortis.mongodb.articles") // servlet mapping
+                .register(MultiPartFeature.class) // multi-part MIME type support
+                .register(ObjectMapperProvider.class)
+                .register(JacksonFeature.class);
+//        rc.register(new LoggingFilter());
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+
+
+        server.stop();
+    }
+}
+
